26ca43cd8666e86ed05f5a4ecb64908c41051207
shashank8983 <mandlik.shashank@gmail.com>
2018-10-24 15:03:11 -0400

    changes in home

--- CRUD_Mvc_CrudRepository_git/src/main/java/com/controller/HomeController.java
+++ CRUD_Mvc_CrudRepository_git/src/main/java/com/controller/HomeController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
+	
 	@RequestMapping("/")
 	public String Start()
 	{
+		System.out.println("bgcghgbjvnbgmn nghvn");
 		return "index";
 		
 	}
