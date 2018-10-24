5be08dc9ced8d04c6216d5362ed2349a3bef03f3
shashank8983 <mandlik.shashank@gmail.com>
2018-10-24 15:06:45 -0400

    aaaaaaaa

--- CRUD_Mvc_CrudRepository_git/src/main/java/com/controller/HomeController.java
+++ CRUD_Mvc_CrudRepository_git/src/main/java/com/controller/HomeController.java
@@ -10,7 +10,7 @@ public class HomeController {
 	@RequestMapping("/")
 	public String Start()
 	{
-		System.out.println("bgcghgbjvnbgmn nghvn");
+		System.out.println("bgcghgbjvnbnghvn");
 		return "index";
 		
 	}
