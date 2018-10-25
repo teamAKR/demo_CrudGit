88cbe3a2730bc48734dc092a51801e0ac9927f81
anuja2137 <anujaraut16@gmail.com>
2018-10-25 10:12:22 +0530

    change

---
+++ CRUD_Mvc_CrudRepository_git/src/main/java/com/model/Student.java
@@ -0,0 +1,43 @@
+package com.model;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student
+{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private int pid;
+	private String name,uname,pass;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+
+}
