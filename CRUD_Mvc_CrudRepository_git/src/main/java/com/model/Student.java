319bdfb8ec402920348c483b6cfde77f6e51e8d6
anuja2137 <44108387+anuja2137@users.noreply.github.com>
2018-10-25 10:27:13 +0530

    Update Student.java

--- CRUD_Mvc_CrudRepository_git/src/main/java/com/model/Student.java
+++ CRUD_Mvc_CrudRepository_git/src/main/java/com/model/Student.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.Id;import javax.persistence;
 
 @Entity
 public class Student
